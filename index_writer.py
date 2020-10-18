78fc7e08e7fc77e66454a0ceb511cb225b1d40f1
Peter Bokor <petttr1@gmail.com>
2020-10-18 20:00:25 +0200

    fix duplicate alt name entries in index

--- index_writer.py
+++ index_writer.py
@@ -1,7 +1,5 @@
 import time
 from collections import OrderedDict
-import json
-import os
 
 
 class IndexWriter:
@@ -15,8 +13,6 @@ class IndexWriter:
 
     def __init__(self, index_file, logging):
         self.index_file = index_file
-        self.tmp_file = "".join(self.index_file.split('\\')[
-                                :-1]) + '\\temp_index.txt'
         self.logging = logging
 
     def write(self, queue):
@@ -27,15 +23,14 @@ class IndexWriter:
             queue (multiprocessing.Queue): Queue used to read processed data
         """
         # Open the file for writing
-        with open(self.tmp_file, mode='w', encoding='utf-8') as file:
+        with open(self.index_file, mode='w', encoding='utf-8') as file:
             while True:
                 try:
                     # get an entry from queue
                     entry = queue.get()
                     # if it is a kill signal quit
                     if entry == 'kill process':
-                        break
-
+                        return
                     # else write the data to a file
                     file.write("""{}:{} \n""".format(
                         entry['title'].strip(' []\n'), entry['alt_title'].strip(' []\n')))
@@ -46,24 +41,3 @@ class IndexWriter:
                 except Exception as e:
                     print('QUEUE EMPTY', e)
                     time.sleep(1)
-
-        self.__cleaup_index()
-        return
-
-    def __cleaup_index(self):
-        # build a dictionary from entries in the file changing the mapping
-        with open(self.tmp_file, mode='r', encoding='utf-8') as file:
-            index_dict = {}
-            for line in file:
-                try:
-                    alt_title, title = line.strip(' []\n').split(':')
-                except:
-                    print(line, line.split(':'))
-                    continue
-                try:
-                    index_dict[title].append(alt_title)
-                except:
-                    index_dict[title] = [alt_title]
-        with open(self.index_file, mode='w+', encoding='utf-8') as file:
-            json.dump(index_dict, file, ensure_ascii=False)
-        os.remove(self.tmp_file)
--- run_system.py
+++ run_system.py
@@ -2,7 +2,8 @@ from xml_processor import XMLProcessor
 from index_writer import IndexWriter
 from worker import Worker
 from multiprocessing import Pool, Process, Manager, Queue
-
+import json
+import os
 
 NUM_WORKERS = 4
 LOGGING = False
@@ -16,31 +17,56 @@ def run(args):
     w.do_work()
 
 
+def cleanup_index(line):
+    try:
+        alt_title, title = line.strip(' []\n').split(':')
+        return alt_title, title
+    except:
+        return None, None
+
+
 if __name__ == '__main__':
+    man = Manager()
+    index_dict = dict()
     if LANG == 'svk':
         wiki_dump = 'wiki_dump\\skwiki-latest-pages-articles-multistream.xml.bz2'
         index_file = 'indices\\skwiki_index.json'
+        tmp_file = 'indices\\svk_tmp_index.txt'
     elif LANG == 'eng':
         # wiki_dump = 'wiki_dump\\enwiki-20201001-pages-articles1.xml-p1p41242.bz2'
         wiki_dump = 'wiki_dump\\enwiki-latest-pages-articles-multistream.xml.bz2'
         index_file = 'indices\\enwiki_index.json'
+        tmp_file = 'indices\\eng_tmp_index.txt'
     else:
         raise Exception("Language not supported")
 
-    m = Manager()
-    xml_queue = m.Queue()
-    index_queue = m.Queue()
-    xml_processor = Process(
-        target=XMLProcessor(
-            wiki_dump, NUM_WORKERS).parse_xml, args=(xml_queue,))
-    index_writer = Process(
-        target=IndexWriter(
-            index_file, LOGGING).write, args=(index_queue,))
-    args = [{'id': i, 'xml_queue': xml_queue, 'index_queue': index_queue}
-            for i in range(NUM_WORKERS)]
-    xml_processor.start()
-    index_writer.start()
-    with Pool(processes=NUM_WORKERS) as p:
-        p.map(run, args)
-    xml_processor.join()
-    index_writer.join()
+    # xml_queue = man.Queue()
+    # index_queue = man.Queue()
+    # xml_processor = Process(
+    #     target=XMLProcessor(
+    #         wiki_dump, NUM_WORKERS).parse_xml, args=(xml_queue,))
+    # index_writer = Process(
+    #     target=IndexWriter(
+    #         tmp_file, LOGGING).write, args=(index_queue,))
+    # args = [{'id': i, 'xml_queue': xml_queue, 'index_queue': index_queue}
+    #         for i in range(NUM_WORKERS)]
+    # xml_processor.start()
+    # index_writer.start()
+    # with Pool(processes=NUM_WORKERS) as p:
+    #     p.map(run, args)
+    # xml_processor.join()
+    # index_writer.join()
+    print('SORTING INDEX')
+    p = Pool(NUM_WORKERS)
+    with open(tmp_file, mode='r', encoding='utf-8') as file:
+        for alt_title, title in p.map(cleanup_index, file):
+            if alt_title != None and title != None:
+                try:
+                    if alt_title not in index_dict[title]:
+                        index_dict[title].append(alt_title)
+                except:
+                    index_dict[title] = [alt_title]
+
+    with open(index_file, mode='w+', encoding='utf-8') as file:
+        json.dump(index_dict, file, ensure_ascii=False)
+    # os.remove(tmp_file)
